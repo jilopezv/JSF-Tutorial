0dc8e25447faf91258ef726f2a4a7d045eac8ed7
Jose Lopez <josei.lopez.velez@gmail.com>
2018-04-09 10:24:52 -0500

    Hito 2: Creación del esquema básico de navegación: Bean (controller) y vistas.

---
+++ src/Navigator.java
@@ -0,0 +1,12 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Navigator {
+	public String choosePage() {
+		if (Math.random() > 0.5) {
+			return ("result-page-1");
+		} else {
+			return ("result-page-2");
+		}
+	}
+}
