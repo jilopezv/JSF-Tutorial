30fc1f91dc2c04f7366c8723ae4b770583596488
Jose Lopez <josei.lopez.velez@gmail.com>
2018-04-19 21:25:39 -0500

    Hito 7: uso de ajax para invocación parcial al servidor

--- JSF-Sample-App/src/co/udem/is3/jsfsample/utils/FormUtils.java
+++ JSF-Sample-App/src/co/udem/is3/jsfsample/utils/FormUtils.java
@@ -8,5 +8,19 @@ public class FormUtils {
 		}
 		return false;
 	}
+	
+
+	  /** Returns the word "copy" in singular or plural. That is, if n is
+	   *  1, returns "copy" (for use in phrase "1 copy"), otherwise returns
+	   *  "copies" (for use in phrases like "2 copies").
+	   */
+	  public static String wordForCopies(int n) {
+	    if (n == 1) {
+	      return("copy");
+	    } else {
+	      return("copies");
+	    }
+	  }
+
 
 }
